fc926183c1c57e9b8fa0a4e45b035b8fca71da30
Wagner Xu <wagnerhsu@hotmail.com>
2025-03-29 19:09:13 +0800

    feat: add files

--- MyCode/05-ComponentAPI/component-api/src/App.vue
+++ MyCode/05-ComponentAPI/component-api/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import MyMessageComponent from './components/MyMessageComponent.vue'
 </script>
 
 <template>
@@ -13,7 +13,7 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
-    <TheWelcome />
+    <MyMessageComponent />
   </main>
 </template>
 
